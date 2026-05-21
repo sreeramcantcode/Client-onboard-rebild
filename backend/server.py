13d9b7787d269d7c47f2abb6a624d86355cbc6c8
Sreeram Sunthar <sreeramoffl1920@gmail.com>
2026-05-21 22:31:40 +0530

    CORS error fix

--- backend/server.py
+++ backend/server.py
@@ -828,8 +828,10 @@ app.include_router(api)
 
 app.add_middleware(
     CORSMiddleware,
+    allow_origins=[
+        os.environ.get("FRONTEND_URL", "")
+    ],
     allow_credentials=True,
-    allow_origins=[os.environ.get("FRONTEND_URL", "*")],
     allow_methods=["*"],
     allow_headers=["*"],
 )
